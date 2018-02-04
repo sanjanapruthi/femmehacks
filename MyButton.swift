c3105330d3eb7bc8f9c0b87cb4ee1daabe8f6b08
sanjanapruthi <33365332+sanjanapruthi@users.noreply.github.com>
2018-02-03 21:09:08 -0500

    Add files via upload

---
+++ MyButton.swift
@@ -0,0 +1,32 @@
+//
+//  MyButton.swift
+//  FemmeHacks
+//
+//  Created by Sanjana Pruthi on 2/3/18.
+//  Copyright © 2018 Sanjana Pruthi and Sruthi Sudhakar. All rights reserved.
+//
+
+import UIKit
+
+//class MyButton: UIButton {
+
+    @IBDesignable class MyButton: UIButton
+    {
+        override func layoutSubviews() {
+            super.layoutSubviews()
+            
+            updateCornerRadius()
+        }
+        
+        @IBInspectable var rounded: Bool = false {
+            didSet {
+                updateCornerRadius()
+            }
+        }
+        
+        func updateCornerRadius() {
+            layer.cornerRadius = rounded ? frame.size.height / 2.5 : 0
+        }
+    }
+
+//}
